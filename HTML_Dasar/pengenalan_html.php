0b042523bdc1d5457afe336b7c426caaa0a5cf8e
Dinda Arum Anggraini <dindaarm512@gmail.com>
2026-08-04 13:39:00 +0700

    Initial commit

---
+++ HTML_Dasar/pengenalan_html.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            pengenalan HTML
+        </title>
+    <head>
+        <body>
+            <h2 align="center">
+                WEB PENGENALAN HTML
+            </h2>
+
+        </body>
+<html>
